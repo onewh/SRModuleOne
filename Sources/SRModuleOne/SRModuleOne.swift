2a60195c1e2d75af28c2095a5800cd5c47ea86da
onehao <onewanghao@gmail.com>
2020-12-05 18:11:28 +0800

    commit files

--- Sources/SRModuleOne/SRModuleOne.swift
+++ Sources/SRModuleOne/SRModuleOne.swift
@@ -1,3 +1,23 @@
-struct SRModuleOne {
-    var text = "Hello, World!"
+import UIKit
+import SwiftRouter
+
+extension ModuleOneTarget: Routeable {
+    public var handler: DispatchWorkItem {
+        switch self {
+        case .listPage:
+            let w = DispatchWorkItem.init {
+                let v = UIViewController()
+                UIApplication.shared.keyWindow?.rootViewController?.present(v, animated: true, completion: nil)
+            }
+            return w
+        default:
+            let w = DispatchWorkItem.init { }
+            return w
+        }
+    }
+    
+    
 }
+
+
+
